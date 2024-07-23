e2c93a92f31755f01b4cfe6206d92a7e8b04e3f8
Dominic <dominicdutchboy@gmail.com>
2024-07-24 01:58:35 +0200

    removed helper function from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,48 +1,22 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
-	"net/http"
-
-	"github.com/Dom-HTG/gin/models"
+	"github.com/Dom-HTG/gin/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 type ProductStore interface {
-	Get(id int)
+	Get()
 	List()
 	Add()
 	Update()
 	Delete()
 }
 
-func dummydata() ([]models.Product, error) {
-	URL := "https://dummyjson.com/products"
-	res, err := http.Get(URL)
-	if err != nil {
-		return nil, err
-	}
-
-	defer res.Body.Close()
-
-	response, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var newProduct []models.Product
-	if err := json.Unmarshal(response, &newProduct); err != nil {
-		return nil, err
-	}
-
-	return newProduct, nil
-}
-
 func main() {
 	router := gin.Default()
 
-	router.GET("/home", handlers.homeHandler)
+	router.GET("/home", handlers.HomeHandler)
 	router.GET("/products", handlers.ListProducts)
 	router.GET("/products/:id", handlers.ListProduct)
 	router.POST("/products/:id", handlers.AddProduct)
