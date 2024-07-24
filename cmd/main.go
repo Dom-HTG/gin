7310f69aa9d2d3279a483812cd39663ddeb6fe56
Dominic <dominicdutchboy@gmail.com>
2024-07-24 15:49:28 +0200

    Added: environment variables & gitignore to ignore the .env file

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/Dom-HTG/gin/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	//Load environment variables.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//get environment variables.
+	PORT := os.Getenv("PORT")
+
+	//Instantiate Router
 	router := gin.Default()
 
 	router.GET("/home", handlers.HomeHandler)
@@ -15,5 +29,5 @@ func main() {
 	router.PUT("/products/:id", handlers.UpdateProduct)
 	router.DELETE("/products/:id", handlers.DeleteProduct)
 
-	router.Run(":4030")
+	router.Run(PORT)
 }
