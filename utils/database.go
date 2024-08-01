bd008b582d42df193fa9f633b1907d1e265a0f2c
Dominic <dominicdutchboy@gmail.com>
2024-08-01 17:42:57 +0200

    added utils/database connection function.

--- utils/database.go
+++ utils/database.go
@@ -1,9 +1,20 @@
 package utils
 
-//initialize and return database connection.
-func initDatabase() (gorm.DB, error) {
-	if conn, err := gorm.open(postgres.open(), gorm.options{}); err != nil {
+import (
+	"os"
+
+	"github.com/Dom-HTG/gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// initialize and return database connection.
+func InitializeDatabase() (*gorm.DB, error) {
+	PG_URL := os.Getenv("PG_URL")
+	conn, err := gorm.Open(postgres.Open(PG_URL), &gorm.Config{})
+	if err != nil {
 		return nil, err
 	}
+	conn.AutoMigrate(&models.Product{})
 	return conn, nil
 }
