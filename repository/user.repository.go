642ece47184178048fcd098d709d2d77e7a1f312
Dominic <dominicdutchboy@gmail.com>
2024-08-01 16:36:01 +0200

    implemented: repository layer

--- repository/user.repository.go
+++ repository/user.repository.go
@@ -1,6 +1,9 @@
 package repository
 
-import "github.com/Dom-HTG/gin/models"
+import (
+	"github.com/Dom-HTG/gin/models"
+	"gorm.io/gorm"
+)
 
 type RepositoryContainer interface {
 	GetProductByID(id int) (models.Product, error)
@@ -10,26 +13,58 @@ type RepositoryContainer interface {
 	DeleteProduct(id int) error
 }
 
-type UserRepository struct {
-	DB *db.DB
+type RepoDependencies struct {
+	DBConn *gorm.DB
 }
 
-func (r *UserRepository) GetProductByID(id int) (models.Product, error) {
-	//database logic here.
+func NewRepoDependencies(DBConn *gorm.DB) *RepoDependencies {
+	return &RepoDependencies{
+		DBConn: DBConn,
+	}
 }
 
-func (r *UserRepository) GetAllProducts() ([]models.Product, error) {
-	//database logic here.
+func (r *RepoDependencies) GetProductByID(id int) (models.Product, error) {
+	var product models.Product
+
+	results := r.DBConn.Where("id = ?", id).Find(&product)
+	if results.Error != nil {
+		return models.Product{}, results.Error
+	}
+	return product, nil
 }
 
-func (r *UserRepository) AddProduct(product models.Product) error {
-	//database logic here.
+func (r *RepoDependencies) GetAllProducts() ([]models.Product, error) {
+	var products []models.Product
+
+	results := r.DBConn.Find(&products)
+	if results.Error != nil {
+		return []models.Product{}, results.Error
+	}
+	return products, nil
 }
 
-func (r *UserRepository) UpdateProduct(id int, product models.Product) error {
-	//database logic here.
+func (r *RepoDependencies) AddProduct(product models.Product) error {
+	results := r.DBConn.Create(&product)
+	if results.Error != nil {
+		return results.Error
+	}
+	return nil
 }
 
-func (r *UserRepository) DeleteProduct(id int) error {
-	//database logic here.
+func (r *RepoDependencies) UpdateProduct(id int, product models.Product) error {
+	results := r.DBConn.Where("id = ?", id).Updates(&product)
+	if results.Error != nil {
+		return results.Error
+	}
+	return nil
+}
+
+func (r *RepoDependencies) DeleteProduct(id int) error {
+	var product models.Product
+
+	results := r.DBConn.Where("id = ?", id).Delete(&product)
+	if results.Error != nil {
+		return results.Error
+	}
+	return nil
 }
