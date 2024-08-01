cd594437b86ecc5aa2124c8aa2175092da803b6a
Dominic <dominicdutchboy@gmail.com>
2024-08-01 17:46:08 +0200

    completed: repo/service/controller layer

--- cmd/main.go
+++ cmd/main.go
@@ -5,33 +5,45 @@ import (
 
 	controller "github.com/Dom-HTG/gin/controllers"
 	"github.com/Dom-HTG/gin/models"
+	"github.com/Dom-HTG/gin/repository"
+	"github.com/Dom-HTG/gin/services"
+	"github.com/Dom-HTG/gin/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+
 	//Load environment variables.
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//database connection.
+	db, err := utils.InitializeDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	//Instance for injecting dependencies
-	productSample := &controller.ProductSample{}
+	productRepo := repository.NewRepoDependencies(db)
+	productService := services.NewServiceDependency(productRepo)
+	productController := controller.NewControllerDependencies(productService)
 
 	//Instantiate gin Router and group routes.
 	router := gin.Default()
 
-	router.GET("/home", productSample.HomeHandler)
-	router.GET("/products", productSample.ListProducts)
-	router.GET("/products/:id", productSample.ListProduct)
+	router.GET("/home", productController.HomeHandler)
+	router.GET("/products", productController.ListProducts)
+	router.GET("/products/:id", productController.ListProduct)
 
 	//All endpoints that allows requests to be mutated are 'protected'.
 	protected := router.Group("/api/protected")
 	{
-		protected.POST("/products", productSample.AddProduct)
-		protected.PUT("/products/:id", productSample.UpdateProduct)
-		protected.DELETE("/products/:id", productSample.DeleteProduct)
+		protected.POST("/products", productController.AddProduct)
+		protected.PUT("/products/:id", productController.UpdateProduct)
+		protected.DELETE("/products/:id", productController.DeleteProduct)
 	}
 
 	router.Run(models.Config.Port)
